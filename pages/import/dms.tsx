358ebedfa17f6b356ffcea79abdd1ab717bf4809
Sarthak Verma <55709030+sarthakvdev@users.noreply.github.com>
2022-12-04 02:44:09 +0530

    DMS page UI

--- pages/import/dms.tsx
+++ pages/import/dms.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from 'next';
 import { useState } from 'react';
 import { useContractWrite, usePrepareContractWrite } from 'wagmi';
+import BackButton from '../../components/BackButton';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { Layout } from '../../components/Layout';
@@ -22,18 +23,20 @@ const Page: NextPage = () => {
 
   return (
     <Layout>
-      <h1 className="text-2xl font-bold">Import wallet using DMS</h1>
-
-      <div className="flex flex-col gap-y-2">
-        <label htmlFor="walletContractAddress">Wallet contract address</label>
-        <Input
-          id="walletContractAddress"
-          type="text"
-          value={walletContractAddress}
-          onChange={(e) => setWalletContractAddress(e.target.value)}
-        />
-
-        <Button variant="primary" onClick={submitRequest}>
+      <BackButton />
+      <div className="flex flex-col gap-y-5 mt-10">
+        <p className="text-2xl font-bold">Import wallet using DMS</p>
+        <div>
+          <label htmlFor="walletContractAddress">Wallet contract address</label>
+          <Input
+            id="walletContractAddress"
+            className='mt-2 w-full'
+            type="text"
+            value={walletContractAddress}
+            onChange={(e) => setWalletContractAddress(e.target.value)}
+          />
+        </div>
+        <Button variant="primary" className='max-w-fit' onClick={submitRequest}>
           Request Recovery
         </Button>
       </div>
