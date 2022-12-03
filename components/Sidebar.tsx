cd4b56c2b9dbea9e224a390e76ba173ff32e02ad
Sarthak Verma <55709030+sarthakvdev@users.noreply.github.com>
2022-12-03 21:02:20 +0530

    sidebar done

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { UserGroupIcon } from '@heroicons/react/24/solid';
+import { FiSend } from 'react-icons/fi';
 import { useRouter } from 'next/router';
 import { FC } from 'react';
 import { truncateWalletAddress } from '../utils';
@@ -11,15 +13,15 @@ interface IWalletCardProps {
 // a simple card that displays the tag, the truncated address and a dummy balance in dollars
 const WalletCard: FC<IWalletCardProps> = ({ address, tag }) => {
   return (
-    <div className="flex flex-col items-center justify-center p-4 space-y-2 bg-white rounded shadow">
-      <div className="flex flex-col items-center justify-center space-y-1">
-        <h1 className="text-lg font-bold">{tag}</h1>
-        <p className="text-sm text-gray-600">
+    <div className="flex flex-col items-start justify-center p-4 space-y-2 bg-white border border-gray-300 rounded-md w-full">
+      <div className="flex flex-col space-y-1">
+        <p className="text-sm font-normal">{tag}</p>
+        <p className="text-sm text-gray-500">
           {truncateWalletAddress(address)}
         </p>
       </div>
       <div className="flex items-center justify-center space-x-1">
-        <p className="text-sm text-gray-600">$0.00</p>
+        <p className="text-lg text-gray-800 font-normal">$0.00</p>
       </div>
     </div>
   );
@@ -38,41 +40,63 @@ export const Sidebar = () => {
 
   return (
     <div className="flex flex-col flex-1 min-h-0 text-neutral-900 border-r border-neutral-200">
-      <div className="flex flex-col flex-1 pt-5 pb-4 overflow-y-auto">
-        <div>
-          <span className="block">All accounts</span>
-          <span className="block">$69.69</span>
+      <div className="flex flex-col flex-1 px-4 pt-5 pb-4 overflow-y-auto">
+        <div className="flex gap-x-4 items-center mb-2 pl-2 py-1 rounded-lg hover:bg-gray-100">
+          <div className="rounded-full p-2 h-max bg-gray-100">
+            <UserGroupIcon className="w-6 text-gray-800" />
+          </div>
+          <div className="flex flex-col">
+            <span className="" text-base>
+              All accounts
+            </span>
+            <span className="text-lg">$69.69</span>
+          </div>
         </div>
 
-        <div className="flex">
-          <Button>Send</Button>
-          <Button>Receive</Button>
+        {/* Send and receive buttons */}
+        <div className="flex gap-x-3">
+          <button className="sidebar-user-btn">
+            <FiSend className="w-3" />
+            Send
+          </button>
+          <button className="sidebar-user-btn">Receive</button>
         </div>
 
-        <hr />
-
-        <div className="flex flex-col flex-grow p-4 space-y-4 overflow-y-auto">
+        {/* Wallets of the user */}
+        <div className="flex flex-col flex-grow py-4 space-y-4 overflow-y-auto">
           <WalletCard
             address="0x1234567890123456789012345678901234567890"
-            tag="My Wallet"
+            tag="Wallet #1"
           />
           <WalletCard
             address="0x1234567890123456789012345678901234567890"
-            tag="My Wallet"
+            tag="Wallet #2"
           />
         </div>
 
-        {/* a settings button at the bottom */}
-        <Button
-          onClick={openDashboard}
-          variant="secondary"
-          className="mt-auto mb-0"
-        >
-          Dashboard
-        </Button>
-        <Button onClick={openSettings} variant="secondary" className="mt-4">
-          Settings
-        </Button>
+        {/* Settings and Dashboard buttons */}
+        <div className="flex flex-col gap-y-2 mb-5">
+          <button
+            onClick={openDashboard}
+            className={`p-2 block rounded-md ${
+              router.pathname === '/unlocked'
+                ? 'bg-gray-200'
+                : 'bg-white hover:bg-gray-200'
+            }`}
+          >
+            Portfolio
+          </button>
+          <button
+            onClick={openSettings}
+            className={`p-2 block rounded-md ${
+              router.pathname === '/unlocked/settings'
+                ? 'bg-gray-200'
+                : 'bg-white hover:bg-gray-200'
+            }`}
+          >
+            Settings
+          </button>
+        </div>
       </div>
     </div>
   );
