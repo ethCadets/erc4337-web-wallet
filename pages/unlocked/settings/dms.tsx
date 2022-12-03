e159bdc0157010b1ddc846dd8c77e209217637f4
Dhaiwat / dhai.eth <dhaiwatpandya@gmail.com>
2022-12-04 01:37:45 +0530

    switch to user-ops for dms setup

--- pages/unlocked/settings/dms.tsx
+++ pages/unlocked/settings/dms.tsx
@@ -1,6 +1,11 @@
 import { NextPage } from 'next';
 import { useState } from 'react';
-import { useContractWrite, usePrepareContractWrite } from 'wagmi';
+import {
+  useContractWrite,
+  usePrepareContractWrite,
+  useProvider,
+  useSigner,
+} from 'wagmi';
 import { Button } from '../../../components/Button';
 import { Input } from '../../../components/Input';
 import { Layout } from '../../../components/Layout';
@@ -9,23 +14,36 @@ import {
   WALLET_CONTRACT_ADDRESS,
 } from '../../../constants';
 import { useDMS } from '../../../hooks/useDMS';
+import { wrapProvider } from '@account-abstraction/sdk';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { Contract, Signer } from 'ethers';
 
 const Page: NextPage = () => {
   const [beneficiaryAddress, setBeneficiaryAddress] = useState('');
   const [timestampDiff, setTimestampDiff] = useState(0);
 
-  const { config } = usePrepareContractWrite({
-    address: WALLET_CONTRACT_ADDRESS,
-    abi: WALLET_CONTRACT_ABI,
-    functionName: 'setSwitch',
-    args: [beneficiaryAddress, timestampDiff],
-  });
-  const { write, isSuccess, isLoading, isError } = useContractWrite(config);
-
+  const provider = useProvider();
+  const { data: signer } = useSigner();
+  const sdkConfig = {
+    chainId: 80001,
+    entryPointAddress: '0x2DF1592238420ecFe7f2431360e224707e77fA0E',
+    bundlerUrl: 'https://bundler-production.up.railway.app:3000/rpc',
+  };
   const { doesSwitchAccountExist, switchAccount, triggerTimestamp } = useDMS();
 
   const setSwitch = async () => {
-    write?.();
+    const aaProvider = await wrapProvider(
+      provider as JsonRpcProvider,
+      sdkConfig,
+      signer as Signer
+    );
+    const aaSigner = aaProvider.getSigner();
+    const contract = new Contract(
+      WALLET_CONTRACT_ADDRESS,
+      WALLET_CONTRACT_ABI,
+      aaSigner
+    );
+    await contract.setSwitch(beneficiaryAddress, timestampDiff);
   };
 
   return (
@@ -41,31 +59,31 @@ const Page: NextPage = () => {
           </div>
         )}
 
-        {!doesSwitchAccountExist && (
-          <>
-            <div className="mt-4">
-              <label>Beneficiary address</label>
-              <Input
-                type="text"
-                value={beneficiaryAddress}
-                onChange={(e) => setBeneficiaryAddress(e.target.value)}
-              />
-            </div>
+        {/* {!doesSwitchAccountExist && ( */}
+        <>
+          <div className="mt-4">
+            <label>Beneficiary address</label>
+            <Input
+              type="text"
+              value={beneficiaryAddress}
+              onChange={(e) => setBeneficiaryAddress(e.target.value)}
+            />
+          </div>
 
-            <div className="mt-4">
-              <label>Timestamp difference</label>
-              <Input
-                type="text"
-                value={timestampDiff}
-                onChange={(e) => setTimestampDiff(Number(e.target.value))}
-              />
-            </div>
+          <div className="mt-4">
+            <label>Timestamp difference</label>
+            <Input
+              type="text"
+              value={timestampDiff}
+              onChange={(e) => setTimestampDiff(Number(e.target.value))}
+            />
+          </div>
 
-            <Button onClick={setSwitch} variant="primary" className="mt-4">
-              Set Switch
-            </Button>
-          </>
-        )}
+          <Button onClick={setSwitch} variant="primary" className="mt-4">
+            Set Switch
+          </Button>
+        </>
+        {/* )} */}
       </div>
     </Layout>
   );
