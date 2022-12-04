fa5fd6298412e5c632c3fcd18051653898ac2a36
Dhaiwat / dhai.eth <dhaiwatpandya@gmail.com>
2022-12-04 06:53:03 +0530

    protcol

--- pages/unlocked/settings/protocol-specific/index.tsx
+++ pages/unlocked/settings/protocol-specific/index.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router';
 import { useState, FC } from 'react';
 import { Button } from '../../../../components/Button';
 import { Layout } from '../../../../components/Layout';
-import { PlusIcon } from "@heroicons/react/24/solid";
+import { PlusIcon } from '@heroicons/react/24/solid';
 
 interface IContractCardProps {
   address: string;
@@ -27,6 +27,7 @@ const ContractCard: FC<IContractCardProps> = ({ address }) => {
 };
 
 const Page: NextPage = () => {
+  const router = useRouter();
   const [enteredContractAddress, setEnteredContractAddress] = useState('');
 
   return (
@@ -44,8 +45,17 @@ const Page: NextPage = () => {
             placeholder="Enter a contract address"
           />
 
-          <Button variant="primary" className="mt-2 flex items-center">
-            <PlusIcon className='w-4 mr-2' />Add
+          <Button
+            onClick={() =>
+              router.push(
+                `/unlocked/settings/protocol-specific/configure/${enteredContractAddress}`
+              )
+            }
+            variant="primary"
+            className="mt-2 flex items-center"
+          >
+            <PlusIcon className="w-4 mr-2" />
+            Add
           </Button>
         </div>
 
@@ -54,8 +64,7 @@ const Page: NextPage = () => {
         <div className="flex flex-col mt-4 gap-4">
           <label>Configured contracts</label>
           <ContractCard address="0xF04eee2Fa7dd65913927377C38579F20ED05B2bE" />
-          <ContractCard address="0xF04eee2Fa7dd65913927377C38579F20ED05B2bE" />
-          <ContractCard address="0xF04eee2Fa7dd65913927377C38579F20ED05B2bE" />
+          <ContractCard address="0x923F3250C2c3c29b13921C1665a83Ac835514c65" />
         </div>
       </div>
     </Layout>
